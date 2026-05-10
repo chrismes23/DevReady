fcb93f0f59207203384f9d8df94d08dadfb7984d
chrismes23 <chrismesogitis@gmail.com>
2026-05-10 19:31:58 +0300

    Created Call and Note model

---
+++ src/models/Call.java
@@ -0,0 +1,5 @@
+package models;
+
+public class Call {
+    public String id,direction,from,to,call_type,duration,archived,created_at;
+}
---
+++ src/models/Note.java
@@ -0,0 +1,5 @@
+package models;
+
+public class Note {
+    public String id,call_id,content;
+}
