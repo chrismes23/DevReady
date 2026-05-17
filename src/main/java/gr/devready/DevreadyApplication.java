ee5eca3acd23da9d93014a14733dd2ea06532485
chrismes23 <chrismesogitis@gmail.com>
2026-05-17 21:01:05 +0300

    Add Maven structure

---
+++ src/main/java/gr/devready/DevreadyApplication.java
@@ -0,0 +1,14 @@
+package gr.devready;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevreadyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevreadyApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/gr/devready/DevreadyApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.devready;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevreadyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
